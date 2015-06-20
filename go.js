c3f2fd9d45656e89698c1ab5abf8c546984456a5
binRick <rapidvps@gmail.com>
2015-06-20 01:13:28 -0400

    m

--- go.js
+++ go.js
@@ -12,9 +12,12 @@ var hostFilter = ['ip'];
 vz.containerTypes.register(vz.containers.Node);
 
 var Dorado = new vz.Host('66.35.71.146', []);
-var Cluster = new vz.Cluster(Dorado);
+var Jeffersoncity = new vz.Host('66.35.67.194',[]);
+var dCluster = new vz.Cluster(Dorado);
+var jCluster = new vz.Cluster(Dorado);
 
-console.log(c.green.bgBlack('Cluster Initialized w/', Cluster.hosts.length, ' hosts'));
+console.log(c.green.bgBlack('Cluster Initialized Dorado ', dCluster.hosts.length, ' hosts'));
+console.log(c.green.bgBlack('Cluster Initialized Jeffersoncity ', jCluster.hosts.length, ' hosts'));
 
 var fC = function() {
 /*    var vms = Cluster.getContainersByType(vz.containers.Node);
@@ -23,7 +26,7 @@ var fC = function() {
         var Host = _.pick(vm.host, hostFilter);
         console.log(pj.render(VM));
     });*/
-    var countSoup = new vz.supervisors.Count(Cluster, vz.containers.Node, 2, false);
+//    var countSoup = new vz.supervisors.Count(Cluster, vz.containers.Node, 2, false);
 
 };
 
