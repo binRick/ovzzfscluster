b259bd8280031d5cec930cb70a465b33bb81c287
binRick <rapidvps@gmail.com>
2015-06-20 20:04:23 -0400

    ew# Please enter the commit message for your changes. Lines starting

---
+++ cluster/devCluster.js
@@ -0,0 +1,78 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+program
+    .version('0.0.1')
+    .option('-h, --host [host]', 'Set host', 'Host')
+    .option('-t, --template [template]', 'Specify OS template', 'Template')
+    .option('-c, --count [count]', 'Specify Node Quantity', 'NodeCount')
+    .option('-w, --webserverPort [webserverPort]', 'Enable Webserver Port', 'Webserver')
+    .parse(process.argv);
+
+var vz = require('../openvz-cluster'),
+    _ = require('underscore'),
+    c = require('chalk'),
+    pj = require('prettyjson'),
+    dns = require('dns');
+
+program.template = program.template || 'centos-7-x86_64';
+var vmFilter = ['id', 'ip', 'hostname', 'status', 'vmStatus', 'ipMonitor', 'ostemplate']; //,'exec_queue'];
+var hostFilter = ['ip'];
+vz.containerTypes.register(vz.containers[program.template]);
+
+if (program.webserverPort) {
+    var express = require('express'),
+        bodyParser = require('body-parser');
+
+    var app = new express();
+    app.use(bodyParser());
+    app.use(express.static(__dirname + '/public_html'));
+app.get('/', function(req, res){
+    res.send(JSON.stringify(program));
+});
+}
+if (program.host && program.template && program.count) {
+    if (program.host.split(',').length == 0)
+        var hosts = [host];
+    else
+        var hosts = program.host.split(',');
+    var Hosts = [];
+    ips = [];
+    host = hosts[0];
+    dns.lookup(host, function onLookup(e, ip, family) {
+        dns.lookup(hosts[1], function onLookup(e, ip2, family) {
+            if (e) throw e;
+            console.log(c.green.bgBlack('looking up', host, c.red.bgBlack(ip)));
+            if (ip.split('.').length != 4) throw "ip=" + ip;
+            var Host = new vz.Host(ip, []);
+            var Cluster = new vz.Cluster(Host);
+            console.log(c.green.bgBlack('Cluster Init', ip));
+            var fC = function() {
+                var count = new vz.supervisors.Count(Cluster, vz.containers[program.template], program.count, false);
+                var cHosts = count.getHostsSortByCtnCount();
+                console.log(c.green.bgBlack('Count Host', cHosts[0].containers.length));
+                var Host = _.pick(cHosts[0], hostFilter);
+                var VMs = cHosts[0].containers.map(function(c) {
+                    return _.pick(c, vmFilter);
+                });
+
+                console.log('VMs', pj.render(VMs.length));
+                console.log('Host', pj.render(Host.length));
+                app.get('/Host', function(req, res){
+                    res.send(Host);
+                });
+                app.get('/VMs', function(req, res){
+                    res.send(VMs);
+                });
+                if (program.webserverPort) {
+                    app.listen(program.webserverPort, function(e) {
+                        console.log(c.green.bgBlack('Express listening on port', program.webserverPort));
+
+                    });
+                }
+
+            };
+            setTimeout(fC, 3000);
+        });
+    });
+}
