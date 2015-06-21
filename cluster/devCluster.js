7e21e0c5ee634e161022e04825826aa15c6ae30b
binRick <rapidvps@gmail.com>
2015-06-20 20:36:33 -0400

    new

--- cluster/devCluster.js
+++ cluster/devCluster.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+var MAXCOUNT = 20
 var program = require('commander');
 program
     .version('0.0.1')
@@ -7,6 +8,7 @@ program
     .option('-t, --template [template]', 'Specify OS template', 'Template')
     .option('-c, --count [count]', 'Specify Node Quantity', 'NodeCount')
     .option('-w, --webserverPort [webserverPort]', 'Enable Webserver Port', 'Webserver')
+    .option('-s, --supervisorType [supervisorType]', 'Select Supervisor Type', 'Supervisor')
     .parse(process.argv);
 
 var vz = require('../openvz-cluster'),
@@ -27,11 +29,11 @@ if (program.webserverPort) {
     var app = new express();
     app.use(bodyParser());
     app.use(express.static(__dirname + '/public_html'));
-app.get('/', function(req, res){
-    res.send(JSON.stringify(program));
-});
+    app.get('/', function(req, res) {
+        res.send(JSON.stringify(program));
+    });
 }
-if (program.host && program.template && program.count) {
+if (program.host && program.template && program.count && program.supervisorType) {
     if (program.host.split(',').length == 0)
         var hosts = [host];
     else
@@ -48,20 +50,26 @@ if (program.host && program.template && program.count) {
             var Cluster = new vz.Cluster(Host);
             console.log(c.green.bgBlack('Cluster Init', ip));
             var fC = function() {
-                var count = new vz.supervisors.Count(Cluster, vz.containers[program.template], program.count, false);
-                var cHosts = count.getHostsSortByCtnCount();
-                console.log(c.green.bgBlack('Count Host', cHosts[0].containers.length));
-                var Host = _.pick(cHosts[0], hostFilter);
-                var VMs = cHosts[0].containers.map(function(c) {
-                    return _.pick(c, vmFilter);
+                var Supervisor = new vz.supervisors[program.supervisorType](Cluster, vz.containers[program.template], program.count, false);
+                app.get('/Count/:newcount', function(req, res) {
+                    req.params.newcount = parseInt(req.params.newcount);
+                    if (req.params.newcount < 0 && req.params.newcount > MAXCOUNT)
+                        return res.end(500);
+                    console.log(c.red.bgBlack('Removing current count and replacing with', req.params.newcount));
+                    delete Supervisor;
                 });
 
-                console.log('VMs', pj.render(VMs.length));
-                console.log('Host', pj.render(Host.length));
-                app.get('/Host', function(req, res){
+                app.get('/Host', function(req, res) {
+                    var cHosts = Supervisor.getHostsSortByCtnCount();
+                    var Host = _.pick(cHosts[0], hostFilter);
                     res.send(Host);
                 });
-                app.get('/VMs', function(req, res){
+
+                app.get('/VMs', function(req, res) {
+                    var cHosts = Supervisor.getHostsSortByCtnCount();
+                    var VMs = cHosts[0].containers.map(function(c) {
+                        return _.pick(c, vmFilter);
+                    });
                     res.send(VMs);
                 });
                 if (program.webserverPort) {
